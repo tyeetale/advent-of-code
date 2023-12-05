9d804aef433d35e5afc75cd7da5e2b5b9a2f3a1c
Tyeetale <17817587+tyeetale@users.noreply.github.com>
2023-12-05 22:27:28 +0700

    fix(day 1): add content

--- 2023/day_1/part_1.py
+++ 2023/day_1/part_1.py
@@ -4,12 +4,15 @@ import re
 file = open('puzzle_input.txt', mode="r", buffering=-1, encoding=None, errors=None, newline=None, closefd=True, opener=None)
 content = file.read()
 
+# print(content)
 # read each line and retroactively get just the numbers with regex
 lines = content.split('\n')
 final_numbers = []
+
 for line in lines:
     # create an list of numbers
     line_numbers = re.findall(r'\d', line)
+    # print(line_numbers)
     # get the first and last values
     final_line_numbers = line_numbers[0] + line_numbers[-1]
     final_numbers.append(
--- 2023/day_1/part_2.py
+++ 2023/day_1/part_2.py
@@ -4,6 +4,7 @@ import re
 file = open('puzzle_input.txt', mode="r", buffering=-1, encoding=None, errors=None, newline=None, closefd=True, opener=None)
 content = file.read()
 
+
 spelled_out_numbers = {
     'one': 1,
     'two': 2,
@@ -20,11 +21,12 @@ spelled_out_numbers = {
 # read each line and retroactively get just the numbers with regex
 lines = content.split('\n')
 final_numbers = []
+
 for line in lines:
     # replace spelled out numbers with their digits
     for key, value in spelled_out_numbers.items(): 
         line = line.replace(key, str(value))
-
+        
     # create an list of numbers
     line_numbers = re.findall(r'\d', line)
 
