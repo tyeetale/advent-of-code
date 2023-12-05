d05a9df94b7898873ecbd3a7a0b1425c940638fb
Tyeetale <17817587+tyeetale@users.noreply.github.com>
2023-12-05 21:47:15 +0700

    fix(2023 day 1): replace jupyter notebook with simple script

---
+++ 2023/day 1/main.py
@@ -0,0 +1,20 @@
+import re
+
+# get the file
+file = open('puzzle_input.txt', mode="r", buffering=-1, encoding=None, errors=None, newline=None, closefd=True, opener=None)
+content = file.read()
+
+# read each line and retroactively get just the numbers with regex
+lines = content.split('\n')
+final_numbers = []
+for line in lines:
+    # create an list of numbers
+    line_numbers = re.findall(r'\d', line)
+    # get the first and last values
+    final_line_numbers = line_numbers[0] + line_numbers[-1]
+    final_numbers.append(
+        final_line_numbers
+    )
+
+final_answer = sum(int(x) for x in final_numbers)
+print(final_answer)
